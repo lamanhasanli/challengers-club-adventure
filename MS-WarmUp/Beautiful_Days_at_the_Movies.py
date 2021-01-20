31c9f3e666dee6473cd0ee135e805e88e4981235
lamanhasanli <56435958+lamanhasanli@users.noreply.github.com>
2021-01-20 22:17:47 +0400

    Create Beautiful_Days_at_the_Movies.py

---
+++ MS-WarmUp/Beautiful_Days_at_the_Movies.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the beautifulDays function below.
+def beautifulDays(i, j, k):
+    beautiful_days = 0
+    for day in range(i, j+1):
+        reverse_day = (str(day)[::-1])
+        if not abs(day-int(reverse_day)) % k:
+            beautiful_days += 1
+            
+    return beautiful_days
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ijk = input().split()
+
+    i = int(ijk[0])
+
+    j = int(ijk[1])
+
+    k = int(ijk[2])
+
+    result = beautifulDays(i, j, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
